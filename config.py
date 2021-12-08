af1744e01e7ace8a1fb0581ac71801031be3e1ce
simumn <seimor@126.com>
2021-12-08 16:46:57 +0800

    chore: update config

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ research purposes. Don't try this code if you are a software engineer.'''
 
 # device settings
 #device = 'cuda' # or 'cpu'
-device = 'cpu'
+device = 'cuda'
 import torch
-#torch.cuda.set_device(0)
+torch.cuda.set_device(0)
 #
 # neptune
 neptune_activate = False
